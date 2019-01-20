2b857ce29f086a984d5ac467484dc9260b4dda0e
cchen97 <manjic@uw.edu>
2019-01-19 17:20:27 -0800

    done

--- SimpleDomainModel/SimpleDomainModel/main.swift
+++ SimpleDomainModel/SimpleDomainModel/main.swift
@@ -26,13 +26,89 @@ open class TestMe {
 public struct Money {
   public var amount : Int
   public var currency : String
+    
+  public init(amount a: Int, currency c: String) {
+        amount = a
+        switch c {
+        case "USD","GBP","EUR","CAN":
+            currency = c
+        default:
+            currency = ""
+            print("Currency is not acceptable")
+        }
+    }
   
   public func convert(_ to: String) -> Money {
+    let target = to
+    let from = currency
+    let initial = amount
+    var result : Int
+    switch from {
+    case "USD":
+        switch target{
+        case "GBP":
+            result =  initial / 2
+        case "EUR":
+            result =  initial * 3 / 2
+        case "CAN":
+            result =  initial * 5 / 4
+        default:
+           result =  initial
+        }
+    case "GBP":
+        switch target{
+        case "USD":
+            result =  initial * 2
+        case "EUR":
+            result =  initial * 3
+        case "CAN":
+            result =  initial * 5 / 2
+        default:
+            result =  initial
+        }
+    case "CAN":
+        switch target{
+        case "USD":
+            result =  initial * 4 / 5
+        case "EUR":
+            result =  initial * 5 / 4
+        case "GBP":
+            result =  initial * 2 / 5
+        default:
+            result =  initial
+        }
+    default: //EUR
+    switch target{
+    case "USD":
+        result =  initial * 2 / 3
+    case "CAN":
+        result =  initial * 4 / 5
+    case "GBP":
+        result =  initial / 3
+    default:
+        result =  initial
+    }
+    }
+    return Money(amount: result, currency: target)
   }
-  
+
   public func add(_ to: Money) -> Money {
+    let a = amount
+    let c = currency
+    if to.currency != currency {
+        let converted = Money(amount: a, currency: c).convert(to.currency)
+        return Money(amount: converted.amount + to.amount, currency: to.currency)
+    }
+    return Money(amount: a + to.amount, currency: c)
   }
   public func subtract(_ from: Money) -> Money {
+    let a = amount
+    let c = currency
+    if from.currency != currency {
+        let converted = Money(amount: a, currency: c).convert(from.currency)
+        return Money(amount:from.amount -  converted.amount, currency: from.currency)
+    }
+    return Money(amount: from.amount - a, currency: c)
   }
 }
 
@@ -47,20 +123,34 @@ open class Job {
     case Hourly(Double)
     case Salary(Int)
   }
-  
+
   public init(title : String, type : JobType) {
+    self.title = title
+    self.type = type
   }
-  
+
   open func calculateIncome(_ hours: Int) -> Int {
+    switch type {
+    case .Hourly(let n):
+        return Int(n * Double(hours))
+    case .Salary(let n):
+        return n
+    }
   }
-  
+
   open func raise(_ amt : Double) {
+    switch type {
+    case .Hourly(let n):
+        type = JobType.Hourly(n + amt)
+    case .Salary(let n):
+        type = JobType.Salary(n + Int(amt))
+    }
   }
 }
-
-////////////////////////////////////
-// Person
 //
+//////////////////////////////////////
+//// Person
+////
 open class Person {
   open var firstName : String = ""
   open var lastName : String = ""
@@ -68,42 +158,65 @@ open class Person {
 
   fileprivate var _job : Job? = nil
   open var job : Job? {
-    get { }
-    set(value) {
+    get { return _job }
+    set(newJob) {
+        if age >= 16 {
+            _job = newJob
+        }
     }
   }
-  
+
   fileprivate var _spouse : Person? = nil
   open var spouse : Person? {
-    get { }
-    set(value) {
+    get { return _spouse }
+    set(newSpouse) {
+        if age >= 18 {
+            _spouse = newSpouse
+        }
     }
   }
-  
+
   public init(firstName : String, lastName: String, age : Int) {
     self.firstName = firstName
     self.lastName = lastName
     self.age = age
   }
-  
+
   open func toString() -> String {
+    return "[Person: firstName:\(firstName) lastName:\(lastName) age:\(age) job:\(_job?.type) spouse:\(_spouse?.firstName)]"
   }
 }
-
-////////////////////////////////////
-// Family
 //
+//////////////////////////////////////
+//// Family
+////
 open class Family {
   fileprivate var members : [Person] = []
-  
+
   public init(spouse1: Person, spouse2: Person) {
+    if spouse1._spouse == nil && spouse2._spouse == nil {
+        members.append(spouse1)
+        members.append(spouse2)
+    }
   }
-  
+
   open func haveChild(_ child: Person) -> Bool {
+    if members[0].age >= 21 || members[1].age >= 21 {
+        members.append(child)
+        return true
+    }
+    return false
   }
-  
+
   open func householdIncome() -> Int {
-  }
+    var result = 0
+    for i in members {
+        if i._job != nil{
+            result = result + i.job!.calculateIncome(2000)
+        }
+    }
+    return result
+   }
 }
 
 
